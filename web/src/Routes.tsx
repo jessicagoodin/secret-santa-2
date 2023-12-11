3149c91df526a41690ffed79ae657a949dd2a677
jessicagoodin <im.jessicagoodin@gmail.com>
2023-12-11 06:13:33 -0800

    Set up authentication using self hosted auth thru RW -- still need to connect user to login page if unauthenticated as it's not working correctly.

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,13 +7,22 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, PrivateSet } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>
+    <Router useAuth={useAuth}>
+      <PrivateSet unauthenticated="login">
+        <Route path="/dashboard" page={DashboardPage} name="dashboard" />
+      </PrivateSet>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
